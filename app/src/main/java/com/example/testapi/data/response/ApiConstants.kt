b5cc3fc7956999abd5c21dc9206fcdcfc373f09d
hanif415 <hanifamrullah42@gmail.com>
2024-05-31 18:26:39 +0700

    add splash screen and home screen

--- app/src/main/java/com/example/testapi/data/response/ApiConstants.kt
+++ app/src/main/java/com/example/testapi/data/response/ApiConstants.kt
@@ -1,6 +1,6 @@
 package com.example.testapi.data.response
 
 object ApiConstants {
-    const val AUTHORIZATION_HEADER = "Authorization: Token r8_4MXt7sHxDjgmRFaAZQHjYytgdtI2sj431WcDd"
+    const val AUTHORIZATION_HEADER = "Authorization: Token "
     const val CONTENT_TYPE_HEADER = "Content-Type: application/json"
 }
